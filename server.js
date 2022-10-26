9710ef25b22f036f5a9eab4a58021651929e3629
intern-laptop <justin.c.kirk@gmail.com>
2022-10-26 10:48:05 -0400

    adds cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 require('dotenv').config()
 const PORT = process.env.PORT;
 
